b069e0c81a8bf06f8a2d770ab4efd53e71ed445d
Jason Lee <lwlee2608@gmail.com>
2026-04-25 21:29:38 +0800

    remove Embedding from Completion interface

--- ai.go
+++ ai.go
@@ -94,8 +94,6 @@ type Completion interface {
 		callback StreamCallback) error
 
 	Models(ctx context.Context) (*pagination.Page[openai.Model], error)
-
-	Embedding(ctx context.Context, input string, model string) ([]float32, error)
 }
 
 type Embedding interface {
